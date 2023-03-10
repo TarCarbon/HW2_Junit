cff46d978cbff50935cfc30069764c6eaf8bb625
root <metalcarbon58@gmail.com>
2023-03-10 15:46:08 +0100

    fixed

--- src/main/java/org/example/SumCalculator.java
+++ src/main/java/org/example/SumCalculator.java
@@ -1,7 +1,19 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.IOException;
+
+public class SumCalculator {
+    public static void main(String[] args) throws IOException {
+
+        SumCalculator sumCalculator = new SumCalculator();
+        System.out.println("set current n: ");
+        System.out.println(sumCalculator.sum(System.in.read()));
+    }
+    int sum (int current) throws IllegalArgumentException {
+        if (current == 0) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = current - 1; i >= 0; --i) current+=i;
+        return current;
     }
 }
--- src/test/java/org/example/SumCalculatorTest.java
+++ src/test/java/org/example/SumCalculatorTest.java
@@ -1,2 +1,34 @@
-package org.example;public class SumCalculatorTest {
+package org.example;
+
+import org.example.SumCalculator;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SumCalculatorTest {
+
+    SumCalculator sumCalculator;
+    @BeforeEach
+    void createObjectTestClass (){
+       sumCalculator = new SumCalculator();
+    }
+
+    @Test
+    void testSumOne () {
+        System.out.println("<=== Test One EXECUTED ===>");
+        Assertions.assertEquals(1, sumCalculator.sum(1));
+    }
+
+    @Test
+    void testSumTwo () {
+        System.out.println("<=== Test Two EXECUTED ===>");
+        Assertions.assertEquals(6, sumCalculator.sum(3));
+    }
+
+    @Test
+    void testSumShowIllegalArgumentException () {
+        System.out.println("<=== Test Three EXECUTED ===>");
+        Throwable exception = assertThrows(IllegalArgumentException.class, ()-> sumCalculator.sum(0));
+        }
+
 }
